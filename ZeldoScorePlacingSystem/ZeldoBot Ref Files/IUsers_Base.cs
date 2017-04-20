9ace6f85e2cdfd9e2b625d70ab099c3187814630
Dominic <domatrix6@gmail.com>
2017-04-20 15:08:11 -0400

    filling missing files in commit

---
+++ ZeldoScorePlacingSystem/ZeldoBot Ref Files/IUsers_Base.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeldoScorePlacingSystem.ZeldoBot_Ref_Files
+{
+    [JsonObject]
+    public class Users
+    {
+        public string UserName = "";
+        //public Expansions EX;
+        public Inventory INV;
+        public Progress Prog;
+
+        
+        //public Users() { UserName = "Dummy"; INV = new Inventory(); Prog = new Progress(); }
+    }
+}
