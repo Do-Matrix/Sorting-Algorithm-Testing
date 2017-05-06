538c7a3987e5aae19c0cabce183011fc9937559a
Dominic <domatrix6@gmail.com>
2017-05-05 21:23:42 -0400

    added sorts officially

---
+++ Sorting Algorithm Testing/Sort_Types/Microsoft.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sorting_Algorithm_Testing.Tests;
+
+namespace Sorting_Algorithm_Testing.Sort_Types
+{
+    class MSsort : ISort_Base
+    {
+        public List<Test_Results> results = new List<Test_Results>();
+        public long NumOfChecks = 0;
+        public long NumOfSwaps = 0;
+
+        public List<Test_Results> InternalResults
+        {
+            get
+            {
+                return results;
+            }
+        }
+
+        public int[] TimedTest(int[] unsortedArray)
+        {
+            var nameList = new List<int>();
+            nameList.AddRange(unsortedArray);
+            nameList.Sort();
+            return nameList.ToArray();
+        }
+
+        public TransationMetrics TransactionTest(int[] unsortedArray)
+        {
+            TransationMetrics TM = new TransationMetrics();
+
+            var nameList = new List<int>();
+            nameList.AddRange(unsortedArray);
+            TM.NumberOfChecks--;
+            nameList.Sort();
+            TM.NumberOfSwaps--;
+            TM.BytesUsed = GC.GetTotalMemory(true);
+            return TM;
+        }
+
+        public override string ToString()
+        {
+            return "Microsoft Sort";
+        }
+    }
+}
---
+++ Sorting Algorithm Testing/Sort_Types/Quicksort.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sorting_Algorithm_Testing.Tests;
+
+namespace Sorting_Algorithm_Testing.Sort_Types
+{
+    class Quicksort : ISort_Base
+    {
+        public List<Test_Results> results = new List<Test_Results>();
+        public long NumOfChecks = 0;
+        public long NumOfSwaps = 0;
+
+        public List<Test_Results> InternalResults
+        {
+            get
+            {
+                return results;
+            }
+        }
+
+        public int[] TimedTest(int[] unsortedArray)
+        {
+            return QuickSort(unsortedArray);
+        }
+
+        public TransationMetrics TransactionTest(int[] unsortedArray)
+        {
+            TransationMetrics TM = new TransationMetrics();
+            return QuickSortTM(unsortedArray, TM);
+        }
+
+        static int[] QuickSort(int[] elements)
+        {
+            Random rand = new Random();
+            if (elements.Count() < 2) return elements;
+            var pivot = rand.Next(elements.Count());
+            var val = elements[pivot];
+            var lesser = new List<int>();
+            var greater = new List<int>();
+            for (int i = 0; i < elements.Count(); i++)
+            {
+                if (i != pivot)
+                {
+                    if (elements[i].CompareTo(val) < 0)
+                    {
+                        lesser.Add(elements[i]);
+                    }
+                    else
+                    {
+                        greater.Add(elements[i]);
+                    }
+                }
+            }
+
+            var merged = QuickSort(lesser.ToArray());
+            var lmerged = merged.ToList();
+            lmerged.Add(val);
+            lmerged.AddRange(QuickSort(greater.ToArray()));
+            return lmerged.ToArray();
+        }
+
+        static TransationMetrics QuickSortTM(int[] elements, TransationMetrics TM)
+        {
+            Random rand = new Random();
+            if (elements.Count() < 2)
+            {
+                TM.BytesUsed = GC.GetTotalMemory(true);
+                return TM;
+            }
+            var pivot = rand.Next(elements.Count());
+            var val = elements[pivot];
+            var lesser = new List<int>();
+            var greater = new List<int>();
+            for (int i = 0; i < elements.Count(); i++)
+            {
+                TM.NumberOfChecks++;
+                if (i != pivot)
+                {
+                    if (elements[i].CompareTo(val) < 0)
+                    {
+                        TM.NumberOfSwaps++;
+                        lesser.Add(elements[i]);
+                    }
+                    else
+                    {
+                        TM.NumberOfSwaps++;
+                        greater.Add(elements[i]);
+                    }
+                }
+            }
+
+            var merged = QuickSort(lesser.ToArray());
+            var lmerged = merged.ToList();
+            lmerged.Add(val);
+            lmerged.AddRange(QuickSort(greater.ToArray()));
+            TM.BytesUsed = GC.GetTotalMemory(true);
+            return TM;
+        }
+
+        public override string ToString()
+        {
+            return "Quicksort";
+        }
+    }
+}
